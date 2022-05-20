df55f425bf47f26044f09d33cbf1b992fca870db
Methodius <66769377+cyh5757@users.noreply.github.com>
2022-05-20 10:42:54 +0900

    str2bool edited
    str -> bool argparse

--- inference_ensemble.py
+++ inference_ensemble.py
@@ -26,6 +26,13 @@ from tqdm import tqdm
 from datasets import load_metric
 from seqeval.metrics import classification_report
 
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 def define_argparser():
     """
@@ -34,7 +41,7 @@ def define_argparser():
     p = argparse.ArgumentParser()
     p.add_argument('--model_folder', required=True)
     p.add_argument('--test_file', required=True)
-    p.add_argument('--use_AutoTokenizer', type=bool, default=True)
+    p.add_argument('--use_AutoTokenizer', type=str2bool, default=True)
     p.add_argument('--gpu_id', type=int, default=-1) 
     p.add_argument('--batch_size', type=int, default=16)
 
